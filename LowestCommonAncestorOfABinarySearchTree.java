d780c7d27a406e900f4d75a6f02a78587e8b251c
lubozju <lubozju@gmail.com>
2015-08-05 22:18:30 -0700

    235

---
+++ LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,36 @@
+/**
+*Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+ /**
+  *  Complexity: O(log_2(n))
+  */ 
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null) {
+            return root;
+        }
+        
+        if (root.val > p.val) {
+            if (root.val < q.val) {
+                return root;
+            }
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            if (root.val > q.val) {
+                return root;
+            }
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+}
