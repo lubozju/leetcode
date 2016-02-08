a3d7cd0672e99dfe4cc0da4cf3250809141d6feb
Bo Lu <lubo@yahoo-inc.com>
2016-02-08 00:55:55 -0800

    palindorme linked list

---
+++ PalindromeLinkedList.java
@@ -0,0 +1,53 @@
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode tailHead = reverse(slow);
+        while (tailHead != null) {
+            if (tailHead.val != head.val) {
+                return false;
+            }
+            head = head.next;
+            tailHead = tailHead.next;
+        }
+        return true;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        
+        return prev;
+    }
+}
