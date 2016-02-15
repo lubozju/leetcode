333067bb4b495dab8b831c09c480e89fa9faf4e8
Bo Lu <lubo@yahoo-inc.com>
2016-02-14 22:19:07 -0800

    binary tree zigzag level order traversal

---
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,67 @@
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        boolean leftRight = true;
+        
+        while (!stack.isEmpty()) {
+            Stack<TreeNode> tempStack = new Stack<TreeNode>();
+            List<Integer> list = new ArrayList<Integer>();
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                if (leftRight) {
+                    if (node.left != null) {
+                        tempStack.push(node.left);
+                    }
+                    if (node.right != null) {
+                        tempStack.push(node.right);
+                    }
+                } else {
+                    if (node.right != null) {
+                        tempStack.push(node.right);
+                    }
+                    if (node.left != null) {
+                        tempStack.push(node.left);
+                    }
+                }
+            }
+            result.add(list);
+            leftRight = !leftRight;
+            stack = tempStack;
+        }
+        return result;
+    }
+}
