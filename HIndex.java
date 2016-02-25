c3652b0d498bc1f1526513a95c8d944ba939f35e
Bo Lu <lubo@yahoo-inc.com>
2016-02-24 22:42:36 -0800

    h index

--- HIndex.java
+++ HIndex.java
@@ -39,3 +39,15 @@ public class Solution {
         return 0;
     }
 }
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for (int i = citations.length; i > 0; i--) {
+            if (citations[citations.length - i] >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
