197fb7c2b2ecaaa4cdc31b796e525efd7647f34d
Bo Lu <lubo@yahoo-inc.com>
2016-03-01 22:15:35 -0800

    palindrom partition 2

--- PalindromePartition2.java
+++ PalindromePartition2.java
@@ -45,3 +45,23 @@ public class Solution {
         return dp[s.length() - 1];
     }
 }
+
+public class Solution {
+    public int minCut(String s) {
+       int[] dp = new int[s.length() + 1];
+       dp[s.length()] = -1;
+       for (int i = 0; i < s.length(); i++) {
+           dp[i] = s.length();
+       }
+       for (int i = s.length() - 1; i >= 0; i--) {
+           for (int a = i, b = i; a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b); a--, b++) {
+               dp[a] = Math.min(dp[a], 1 + dp[b + 1]);
+           }
+           for (int a = i, b = i + 1; a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b); a--, b++) {
+               dp[a] = Math.min(dp[a], 1 + dp[b + 1]);
+           }
+       }
+       
+       return dp[0];
+    }
+}
