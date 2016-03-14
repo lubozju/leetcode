c558678a91190fcc19903393021127f456855023
Bo Lu <lubo@yahoo-inc.com>
2016-03-13 22:21:45 -0700

    number of islands

--- NumberOfIslands.java
+++ NumberOfIslands.java
@@ -71,3 +71,35 @@ public class Solution {
         }
     }
 }
+
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(char[][] grid, int i, int j) {
+        grid[i][j] = '0';
+        if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+            dfs(grid, i - 1, j);
+        }
+        if (i + 1 <= grid.length - 1 && grid[i + 1][j] == '1') {
+            dfs(grid, i + 1, j);
+        }
+        if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+            dfs(grid, i, j - 1);
+        }
+        if (j + 1 <= grid[i].length - 1 && grid[i][j + 1] == '1') {
+            dfs(grid, i, j + 1);
+        }
+    }
+}
