14ad234940f863c07380ea5b0ac8bf91417fc725
Bo Lu <lubo@yahoo-inc.com>
2016-03-20 21:24:49 -0700

    subsets

--- Subsets.java
+++ Subsets.java
@@ -40,3 +40,22 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> list = new ArrayList<Integer>();
+            for (int j = 0; j < n; j++) {
+                if ((1 << j & i) != 0) {
+                    list.add(nums[j]);
+                }
+            }
+            result.add(list);
+        }
+        
+        return result;
+    }
+}
