d3492e02f2168d8fb9814d7e1d4d523b8fd632ce
Bo Lu <lubo@yahoo-inc.com>
2016-05-18 23:40:10 -0700

    intersection of two arrays

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -31,3 +31,39 @@ public class Solution {
         return result;
     }
 }
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<Integer>();
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+                while (i < nums1.length && nums1[i] == nums1[i - 1]) {
+                    i++;
+                }
+                while (j < nums2.length && nums2[j] == nums2[j - 1]) {
+                    j++;
+                }
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        for (int k = 0; k < result.length; k++) {
+            result[k] = list.get(k);
+        }
+        return result;
+    }
+}
