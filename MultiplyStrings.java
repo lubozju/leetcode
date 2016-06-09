d656bf9ffc22939254bc17c59f6276ce4956af1d
Bo Lu <lubo@yahoo-inc.com>
2016-06-08 23:32:13 -0700

    multiply strings

---
+++ MultiplyStrings.java
@@ -0,0 +1,39 @@
+/*
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+Note:
+The numbers can be arbitrarily large and are non-negative.
+Converting the input string to integer is NOT allowed.
+You should NOT use internal library such as BigInteger.
+*/
+
+public class Solution {
+    // Time: O(num1.length() * num2.length())
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int n1 = num1.length(), n2 = num2.length();
+        int[] products = new int[n1 + n2];
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int d1 = num1.charAt(i) - '0';
+                int d2 = num2.charAt(j) - '0';
+                products[i + j + 1] += d1 * d2;
+            }
+        }
+        int carry = 0;
+        for (int i = products.length - 1; i >= 0; i--) {
+            int tmp = (products[i] + carry) % 10;
+            carry = (products[i] + carry) / 10;
+            products[i] = tmp;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int num : products) {
+            if (num != 0 || sb.length() > 0) {
+                sb.append(num);
+            }
+        }
+        return sb.toString();
+    }
+}
