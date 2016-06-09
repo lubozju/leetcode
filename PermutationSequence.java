45acd14c17c910c4a720040ce7f6566024a5d2f5
Bo Lu <lubo@yahoo-inc.com>
2016-06-09 01:00:53 -0700

    permutation sequence

--- PermutationSequence.java
+++ PermutationSequence.java
@@ -13,40 +13,36 @@ We get the following sequence (ie, for n = 3):
 Given n and k, return the kth permutation sequence.
 
 Note: Given n will be between 1 and 9 inclusive.
+
+
 */
 
 public class Solution {
     public String getPermutation(int n, int k) {
-        // make everthing 0 based
-        k = k - 1;
         StringBuilder sb = new StringBuilder();
+        int factorial = 1;
+        for (int i = 2; i <= n; i++) {
+            factorial *= i;
+        }
         boolean[] used = new boolean[n];
-        int facto = 1;
-        for (int i = 2; i < n; i++) {
-            facto *= i;
+
+        k = k - 1;
+        while (n > 0) {
+            factorial /= n;
+            int index = k / factorial;
+            sb.append(find(used, index));
+            k = k % factorial;
+            n--;
         }
         
-        for (int i = 0; i < n - 1; i++) {
-            int index = k / facto;
-            k = k % facto;
-            facto /= (n - 1 - i);
-            
-            int number = find(used, index);
-            sb.append(number);
-        }
-        
-        int number = find(used, 0);
-        sb.append(number);
-    
         return sb.toString();
     }
     
-    // find the indexth unused number (0 based) from (1 - n)
-    private int find(boolean[] used, int index) {
+    private int find (boolean[] used, int index) {
         int count = -1;
         int i = 0;
-        while (count != index) {
-            if (used[i] == false) {
+        while (count < index) {
+            if (!used[i]) {
                 count++;
             }
             i++;
