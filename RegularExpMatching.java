e616a780e512370142befc016212d5092152fcf0
Bo Lu <lubo@yahoo-inc.com>
2016-06-09 21:54:10 -0700

    regular exp matching

--- RegularExpMatching.java
+++ RegularExpMatching.java
@@ -69,3 +69,49 @@ public class Solution {
         return result;
     }
 }
+
+
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        // Time: O(s.length() * p.length())
+        // Time: O(s.length() * p.length())
+        // dp[i][j]: s.substring(i), p.substring(j)
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+        
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                char pc = p.charAt(j);
+                if (pc == '*') {
+                    continue;
+                }
+                if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                    if (i == s.length()) {
+                        dp[i][j] = dp[i][j + 2];
+                    } else {
+                        char sc = s.charAt(i);
+                        if (sc == pc || pc == '.') {
+                            dp[i][j] = dp[i][j + 2] || dp[i + 1][j + 2] || dp[i + 1][j];
+                        } else {
+                            dp[i][j] = dp[i][j + 2];
+                        }
+                    }
+                } else {
+                    if (i == s.length()) {
+                        dp[i][j] = false;
+                    } else {
+                        char sc = s.charAt(i);
+                        if (pc == '.') {
+                            dp[i][j] = dp[i + 1][j + 1];
+                        } else {
+                            dp[i][j] = (pc == sc) && dp[i + 1][j + 1];
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
