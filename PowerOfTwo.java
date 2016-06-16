cc2073d2595bc696b469a1c9f1382be2f96d3420
Bo Lu <lubo@yahoo-inc.com>
2016-06-15 22:34:14 -0700

    power of two

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -10,3 +10,18 @@ public class Solution {
         return (n & (n - 1)) == 0;
     }
 }
+
+
+public class Solution {
+    // Time: O(log(n))
+    // Space: O(1)
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n = n >> 1;
+        }
+        return n == 1;
+    }
+}
