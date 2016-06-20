c6cfc1436d96fb1543fd81e76678c21fe671cfcf
Bo Lu <lubo@yahoo-inc.com>
2016-06-19 23:29:23 -0700

    search in rotated sorted array

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,42 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+*/
+
+public class Solution {
+    // Time: O(log(n))
+    // Space: O(1)
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (target > nums[mid]) {
+                if (nums[mid] >= nums[left] || target < nums[left]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[mid] < nums[right] || target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        if (target == nums[left]) {
+            return left;
+        }
+        return -1;
+    }
+}
