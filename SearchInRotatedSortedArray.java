68c7d3a443bafbbc2602630cf66cf2aa73cdfebd
Bo Lu <lubo@yahoo-inc.com>
2016-07-07 23:09:51 -0700

    search in rotated sorted array

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -40,3 +40,34 @@ public class Solution {
         return -1;
     }
 }
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[mid] > target && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+}
---
+++ SearchInRotatedSortedArray2.java
@@ -0,0 +1,40 @@
+/*
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+*/
+
+public class Solution {
+    // Time O(n)
+    // Space O(1)
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] > nums[right]) {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                right--;
+            }
+        }
+        return nums[left] == target;
+    }
+}
