303516d670dd62594c1b0c1b4751cbc22998940c
Bo Lu <lubo@yahoo-inc.com>
2016-08-03 23:45:01 -0700

    nested list weight sum 2

---
+++ NestedListWeightSum2.java
@@ -0,0 +1,86 @@
+/*
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Different from the previous question where weight is increasing from root to leaf, now the weight is defined from bottom up. i.e., the leaf level integers have weight 1, and the root level integers have the largest weight.
+
+Example 1:
+Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2)
+
+Example 2:
+Given the list [1,[4,[6]]], return 17. (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    // Time O(n)
+    // Space O(1)
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        int depth = findDepth(nestedList);
+        return help(nestedList, depth);
+    }
+    
+    private int findDepth(List<NestedInteger> nestedList) {
+        int depth = 0;
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) {
+                depth = Math.max(depth, 1);
+            } else {
+                depth = Math.max(depth, findDepth(ni.getList()) + 1);
+            }
+        }
+        return depth;
+    }
+    
+    private int help(List<NestedInteger> nestedList, int depth) {
+        int sum = 0;
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) {
+                sum += depth * ni.getInteger();
+            } else {
+                sum += help(ni.getList(), depth - 1);
+            }
+        }
+        return sum;
+    }
+}
+
+public class Solution {
+    // Time O(n)
+    // Space O(1)
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        int sum = 0;
+        int levelSum = 0;
+        while (!nestedList.isEmpty()) {
+            List<NestedInteger> nextLevel = new ArrayList<>();
+            for (NestedInteger ni : nestedList) {
+                if (ni.isInteger()) {
+                    levelSum += ni.getInteger();
+                } else {
+                    nextLevel.addAll(ni.getList());
+                }
+            }
+            nestedList = nextLevel;
+            // old value will be added for every new level
+            sum += levelSum;
+        }
+        return sum;
+    }
+}
