0f091a573efe9efd8722503e954931001b406da8
Bo Lu <lubo@yahoo-inc.com>
2016-08-18 17:07:07 -0700

    strStr

--- StrStr.java
+++ StrStr.java
@@ -23,3 +23,45 @@ public class Solution {
         return true;
     }
 }
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        int hN = 0;
+        int hH = 0;
+        for (int i = 0; i < needle.length(); i++) {
+            hN += needle.charAt(i);
+            hH += haystack.charAt(i);
+        }
+        
+        int start = 0;
+        int end = needle.length() - 1;
+        end++;
+        while (end < haystack.length()) {
+            if (hN == hH) {
+                if (check(haystack, needle, start)) {
+                    return start;
+                }
+            }
+            hH = hH - haystack.charAt(start) + haystack.charAt(end);
+            end++;
+            start++;
+        }
+        if (hN == hH) {
+            if (check(haystack, needle, start)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+    private boolean check(String haystack, String needle, int start) {
+        int i = 0;
+        while (i < needle.length() && haystack.charAt(start + i) == needle.charAt(i)) {
+            i++;
+        }
+        return i == needle.length();
+    }
+    
+}
