107c7ef1cfe26cc34d3389c18b98220da5868a5e
Bo Lu <lubo@yahoo-inc.com>
2016-08-24 21:46:26 -0700

    integer to roman

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -24,3 +24,22 @@ public class Solution {
     }
 }
 
+public class Solution {
+    public String intToRoman(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX","V", "IV", "I"};
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            if (num / nums[i] > 0) {
+                int k = num / nums[i];
+                for (int j = 0; j < k; j++) {
+                    sb.append(romans[i]);
+                }
+                num = num % nums[i];
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
