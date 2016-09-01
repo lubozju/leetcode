b79643aa955130da56ecb76477d3a2fe6316bf1a
Bo Lu <lubo@yahoo-inc.com>
2016-08-31 20:32:53 -0700

    meeting room

---
+++ MeetingRoom.java
@@ -0,0 +1,36 @@
+/*
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return false.
+*/
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+	// Time O(nlogn)
+	// Space O(1)
+    public boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval in1, Interval in2) {
+                return in1.start - in2.start;
+            }
+        });
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
